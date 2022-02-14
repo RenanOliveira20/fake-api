2c5219f1a6b889db148ef00b3dc23f1ec27d5c4e
Renan Oliveira <reluiz@leroymerlin.com.br>
2022-02-14 17:11:46 -0300

    first commit

---
+++ routes.js
@@ -0,0 +1,16 @@
+const routes = `
+/stores/0045/sales/daily -> vendas diárias loja
+/stores/0045/sales/monthly -> vendas mensais loja
+/stores/0045/sales/section/01/daily -> vendas diarias do setor especifico
+/stores/0045/sales/subsection/0101/daily -> vendas diarias da subseção especifica
+/stores/0045/sales/section/01/monthly -> vendas mensais do setor especifico
+/stores/0045/sales/subsection/0101/monthly-> vendas mensais da subseção especifica
+/stores/0045/sales/profile/01 -> vendas dos assessores da seção especifica
+/stores/0045/sales/profile/51023091/daily -> vendas do assessor especifico no dia
+/stores/0045/sales/profile/51023091/monthly ->  vendas do assessor especifico no mes
+/stores/0045/humanResource/employee -> funcionario especifico **
+{
+    /stores/0045/sales/profile/daily -> vendas de todos os assessores da loja no dia 
+    /stores/0045/sales/profile/monthly -> vendas de todos os assessores da loja no mes
+} 
+`
