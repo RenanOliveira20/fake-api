9ee4f9e17afa0a39dcc47793fd1929af05aee47f
christopherfulldev <highlander.all@gmail.com>
2022-02-15 22:26:35 -0300

    Fix dependencies

--- routes.js
+++ routes.js
@@ -1,4 +1,4 @@
-const routes = `
+/*const routes = 
 /stores/0045/sales/daily -> vendas diárias loja
 /stores/0045/sales/monthly -> vendas mensais loja
 /stores/0045/sales/section/01/daily -> vendas diarias do setor especifico
@@ -13,4 +13,4 @@ const routes = `
     /stores/0045/sales/profile/daily -> vendas de todos os assessores da loja no dia 
     /stores/0045/sales/profile/monthly -> vendas de todos os assessores da loja no mes
 } 
-`
+*/
